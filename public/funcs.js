39db1055879cb972f21977a8333330f0e2e71519
Jordan15 <jadavis7776@yahoo.com>
2022-01-26 01:02:50 -0500

    added table logic instead of buttons, will need cleaned up

--- public/funcs.js
+++ public/funcs.js
@@ -28,3 +28,41 @@ function createBoardButtons(){
         }
     }
 }
+
+function createTable(){
+    console.log("creating board table");
+    for(i = 0; i < 5; i++){
+        btnArr[i] = new Array();
+        var tabRow = document.createElement("tr")
+        tabRow.id = 'tr'+i;
+        document.getElementById("boardTable").appendChild(tabRow);
+        for(j = 0; j < 5; j++){
+            btnArr[i][j]= document.createElement("td");
+            btnArr[i][j].id = 'td' + i + j;
+            btnArr[i][j].name = btnArr[i][j].id+'TEST'; //this is the value recieved in the post req
+            btnArr[i][j].textContent = btnArr[i][j].name = btnArr[i][j].id+'TEXTtest';;
+            
+            btnArr[i][j].addEventListener("click", testPost);
+           // btnArr[i][j].addEventListener("click", myfunction(btnArr[i][j].id))
+            //console.log(btnArr[i][j].onclick)
+            
+            document.getElementById("tr" + i).appendChild(btnArr[i][j]);
+            document.getElementById("td" + i + j).className = "tableData";
+        }
+        
+    }
+}
+
+function testPost(){
+    this.name='X';
+    this.innerHTML='X';
+    var buttonList = document.querySelectorAll(".tableData");
+    console.log(buttonList);
+    fetch("/", {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'}, 
+        body: JSON.stringify(buttonList)
+      }).then(res => {
+        console.log("Request complete! response:", res);
+      });
+}
