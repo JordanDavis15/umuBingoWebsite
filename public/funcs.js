8fff69d2b4db17d35a8ffba68d2c95fe063d1248
Jordan15 <jadavis7776@yahoo.com>
2022-03-24 20:45:14 -0400

    fixed funcs.js so all rows are same height

--- public/funcs.js
+++ public/funcs.js
@@ -42,6 +42,18 @@ function createTable(qAndAs){
     }
     document.getElementById('questionbox').textContent = qAndAs[0][questionIndex];
     incrementQuestionIndex();
+    setRowHeight();
+}
+
+function setRowHeight(){
+    var tbl=document.getElementById('boardTable'), biggestRow=0, rowHeight=0, row=0;
+    for (row=0; row < tbl.rows.length; row++) {     //find biggest row height
+        rowHeight=parseInt(tbl.rows[row].offsetHeight);
+        if (rowHeight > biggestRow) {biggestRow=rowHeight;}
+    }
+    for (row=0; row < tbl.rows.length; row++) {     //set all rows to biggest row height
+        tbl.rows[row].style.height=biggestRow + "px";
+    }
 }
 
 
